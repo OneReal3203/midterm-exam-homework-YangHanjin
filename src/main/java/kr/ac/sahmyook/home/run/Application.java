982e5f610bdf2494022e27e0cdabdaef39b63d6b
AzureAD\양한진 <gkswls3203@naver.com>
2023-05-09 21:37:39 +0900

    작성

---
+++ src/main/java/kr/ac/sahmyook/home/run/Application.java
@@ -0,0 +1,12 @@
+package kr.ac.sahmyook.home.run;
+import kr.ac.sahmyook.home.view.*;
+public class Application {
+
+    public static void main(String[] args){
+        Menu calc = new Menu();
+        calc.mainMenu();
+
+
+    }
+
+}
